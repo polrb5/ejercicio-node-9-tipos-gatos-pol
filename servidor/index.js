756d85bfb543a6a1e4fb4b9df1ca06836a7400da
Pol Reig <polreigbroto@gmail.com>
2021-07-14 18:55:53 +0200

    Paquete cors instalado

--- servidor/index.js
+++ servidor/index.js
@@ -1,9 +1,11 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
 const app = require("./init");
 const { errorGeneral, error404 } = require("./errores");
 const rutasTipos = require("./rutas/tipos");
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
